4de49a651200c82dbf8ca3d8b5ef94b5ded01357
20171105122 <20171105122a@admindeiMac-38.local>
2018-06-21 09:58:34 +0800

    gaibiabxunhuan

---
+++ main.cpp
@@ -0,0 +1,17 @@
+///Users/20171105122a/Desktop/untitled folder/text621/text621/main.cpp
+//  main.cpp
+//  text621
+//
+//  Created by 冯佩玲 on 18/6/21.
+//  Copyright © 2018年 冯佩玲. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i;
+    for(i=0;i<100;i++);
+    cout<<i<<endl;
+    return 0;
+}
